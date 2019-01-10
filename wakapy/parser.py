dce569340679d581db4308eacd64e9e52e2ebc4d
surister <surister98@gmail.com>
2019-01-10 21:12:06 +0100

    Update

--- wakapy/parser.py
+++ wakapy/parser.py
@@ -1,15 +1,35 @@
 import pathlib
 import json
 
+from wakapy.days import Day
+
 path = pathlib.PurePath(__file__).parent
 
 
-class Injector:
-    @staticmethod
-    def _user_info_extractor(fp: str):
-        with open(fp, 'r') as f:
-            inf = json.load(f)
-        return inf['user']
+class JsonFile:
+    def __init__(self, fp: str):
+        self.fp = fp
+        self.file = self.load_file()
+        #self._days = [Day(item) for item in self.file['days']]
+        #self.test = Day([self.file['days']])
 
-    def info(self, fp):
-        return self._user_info_extractor(fp)
+        Day(self.file['days'][0])
+
+    def load_file(self):
+        with open(self.fp, 'r') as f:
+            return json.load(f)
+
+    @property
+    def user_info(self) -> dict:
+        return self.file['user']
+
+    @property
+    def days(self):
+        return self._days
+
+    def test(self):
+        return self.file['days']
+
+
+if __name__ == '__main__':
+    a = JsonFile(r'C:\Users\k260g\Desktop\project\info.json')
