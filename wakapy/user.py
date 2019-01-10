f282d5ac8a5f308863a2249a677f1d14ffb29d9b
surister <surister98@gmail.com>
2019-01-10 21:12:34 +0100

    Improved a lot.

--- wakapy/user.py
+++ wakapy/user.py
@@ -1,6 +1,6 @@
 from pathlib import PurePath, Path
 
-from wakapy.parser import Injector
+from wakapy.parser import JsonFile
 from wakapy.exceptions import EmptyFolderError
 
 io_path = PurePath(PurePath(__file__).parent).joinpath('io/')
@@ -14,34 +14,38 @@ class User:
             raise EmptyFolderError('Io folder is empty and no file path was given')
 
         self.fp = files[file_number] if fp is None else fp
-        kw = Injector().info(self.fp)
 
-        self.display_name = kw.get('display_name')
-        self.created_at = kw.get('created_at')
-        self.email = kw.get('email')
-        self.is_email_public = kw.get('is_email_public')
-        self.full_name = kw.get('full_name')
-        self.has_premium_features = kw.get('has_premium_features')
-        self.human_readable_website = kw.get('human_readable_website')
-        self.id = kw.get('id')
-        self.is_email_confirmed = kw.get('is_email_confirmed')
-        self.is_hireable = kw.get('is_hireable')
-        self.languages_used_public = kw.get('languages_used_public')
-        self.last_heartbeat = kw.get('last_heartbeat')
-        self.last_plugin = kw.get('last_plugin')
-        self.last_plugin_name = kw.get('last_plugin_name')
-        self.last_project = kw.get('last_project')
-        self.location = kw.get('location')
-        self.logged_time_public = kw.get('logged_time_public')
-        self.modified_at = kw.get('modified_at')
-        self.photo = kw.get('photo')
-        self.photo_public = kw.get('photo_public')
-        self.plan = kw.get('plan')
-        self.timeout = kw.get('timeout')
-        self.timezone = kw.get('timezone')
-        self.username = kw.get('username')
-        self.website = kw.get('website')
-        self.writes_only = kw.get('writes_only')
+        self.file = JsonFile(self.fp)
+        user_info = self.file.user_info
+
+        self.display_name = user_info.get('display_name')
+        self.created_at = user_info.get('created_at')
+        self.email = user_info.get('email')
+        self.is_email_public = user_info.get('is_email_public')
+        self.full_name = user_info.get('full_name')
+        self.has_premium_features = user_info.get('has_premium_features')
+        self.human_readable_website = user_info.get('human_readable_website')
+        self.id = user_info.get('id')
+        self.is_email_confirmed = user_info.get('is_email_confirmed')
+        self.is_hireable = user_info.get('is_hireable')
+        self.languages_used_public = user_info.get('languages_used_public')
+        self.last_heartbeat = user_info.get('last_heartbeat')
+        self.last_plugin = user_info.get('last_plugin')
+        self.last_plugin_name = user_info.get('last_plugin_name')
+        self.last_project = user_info.get('last_project')
+        self.location = user_info.get('location')
+        self.logged_time_public = user_info.get('logged_time_public')
+        self.modified_at = user_info.get('modified_at')
+        self.photo = user_info.get('photo')
+        self.photo_public = user_info.get('photo_public')
+        self.plan = user_info.get('plan')
+        self.timeout = user_info.get('timeout')
+        self.timezone = user_info.get('timezone')
+        self.username = user_info.get('username')
+        self.website = user_info.get('website')
+        self.writes_only = user_info.get('writes_only')
+
+        self.days = self.file.days
 
     def __repr__(self):
         return f'class <{self.__class__.__name__}({self.username})>'
