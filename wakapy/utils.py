fa3563908d9835a4a489c00ff9c5fb9d8194a0b1
surister <surister98@gmail.com>
2019-01-11 15:17:45 +0100

    Add utils.py plus order dict function

---
+++ wakapy/utils.py
@@ -0,0 +1,11 @@
+
+def order_dict(_dict, reverse=True):
+    temp_dict = {}
+
+    unordered_list = [(k, v) for k, v in _dict.items()]
+    ordered_list = sorted(unordered_list, key=lambda x: x[1] , reverse=reverse)
+
+    for item in ordered_list:
+        temp_dict[item[0]] = item[1]
+
+    return temp_dict
