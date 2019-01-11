94bc197ad85a778cdda28762c12ee2602d12156b
surister <surister98@gmail.com>
2019-01-11 01:43:31 +0100

    Add Project, Branch and Category class

--- wakapy/containers.py
+++ wakapy/containers.py
@@ -7,16 +7,37 @@ class Parent:
         self.percent = _dict.get('percent')
         self.seconds = _dict.get('seconds')
         self.text = _dict.get('text')
-        self.time = _dict.get('total_seconds')
+        self.total_time = _dict.get('total_seconds')
         self.type = _dict.get('type')
 
     def __repr__(self):
         return f"class <'{self.__class__.__name__}({self.name})'>"
 
 
-class Project(Parent):
+class Project:
+    untitled_counter = 0
+
     def __init__(self, _dict):
-        super().__init__(_dict=_dict)
+        self.name = _dict.get('name')
+
+        self.is_untitled = False
+        if self.name == 'untitled':
+            Project.untitled_counter += 1
+            self.is_untitled = True
+        self.num = Project.untitled_counter
+
+        self.branches = [Branch(i) for i in _dict.get('branches')]
+        self.categories = [Category(i) for i in _dict.get('categories')]
+        self.dependencies = [Dependency(i) for i in _dict.get('dependencies')]
+        self.editors = [Editor(i) for i in _dict.get('editors')]
+        self.entities = [Entity(i) for i in _dict.get('entities')]
+        self.languages = [Language(i) for i in _dict.get('languages')]
+        self.operative_systems = [Os(i) for i in _dict.get('operating_systems')]
+
+        self.repr_string = f'-{self.num}' if self.is_untitled else ""
+
+    def __repr__(self):
+        return f"class <'{self.__class__.__name__}({self.name}{self.repr_string})'>"
 
 
 class Os(Parent):
@@ -45,5 +66,10 @@ class Category(Parent):
 
 
 class Editor(Parent):
+    def __init__(self, _dict):
+        super().__init__(_dict=_dict)
+
+
+class Branch(Parent):
     def __init__(self, _dict):
         super().__init__(_dict=_dict)
