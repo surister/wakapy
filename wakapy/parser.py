79b7def0f8c31f641ea6f3c738cf4bbbe2eddac6
surister <surister98@gmail.com>
2019-01-11 01:14:20 +0100

    It now works

--- wakapy/parser.py
+++ wakapy/parser.py
@@ -1,5 +1,6 @@
 import pathlib
 import json
+from typing import List
 
 from wakapy.days import Day
 
@@ -10,26 +11,22 @@ class JsonFile:
     def __init__(self, fp: str):
         self.fp = fp
         self.file = self.load_file()
-        #self._days = [Day(item) for item in self.file['days']]
-        #self.test = Day([self.file['days']])
+        self._days = [Day(item) for item in self.file['days']]
 
         Day(self.file['days'][0])
 
-    def load_file(self):
+    def load_file(self) -> dict:
         with open(self.fp, 'r') as f:
-            return json.load(f)
+            a = json.load(f)
+            return a
 
     @property
     def user_info(self) -> dict:
         return self.file['user']
 
     @property
-    def days(self):
+    def days(self) -> List[Day]:
         return self._days
 
-    def test(self):
-        return self.file['days']
-
-
-if __name__ == '__main__':
-    a = JsonFile(r'C:\Users\k260g\Desktop\project\info.json')
+    def __repr__(self):
+        return f'Wakapy.{self.__class__.__name__}({self.fp})'
