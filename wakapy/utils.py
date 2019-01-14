4df125579cba1bca4f02fefed988e199c75e74db
surister <surister98@gmail.com>
2019-01-14 15:38:01 +0100

    Anotations added

--- wakapy/utils.py
+++ wakapy/utils.py
@@ -1,20 +1,11 @@
-from pathlib import PurePath, Path
-from typing import NamedTuple
 
-
-def order_dict(_dict, reverse=True):
+def order_dict(_dict: dict, reverse: bool=True) -> dict:
     temp_dict = {}
 
     unordered_list = [(k, v) for k, v in _dict.items()]
-    ordered_list = sorted(unordered_list, key=lambda x: x[1] , reverse=reverse)
+    ordered_list = sorted(unordered_list, key=lambda x: x[1], reverse=reverse)
 
     for item in ordered_list:
         temp_dict[item[0]] = item[1]
 
     return temp_dict
-
-
-class Constants(NamedTuple):
-    data_path = PurePath(PurePath(__file__).parent).joinpath('data/')
-    data_files = [file for file in Path(data_path).iterdir()]
-
