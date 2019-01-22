989136857f1474b3b991c5c9f371245391e5ac63
surister <surister98@gmail.com>
2019-01-22 17:59:08 +0100

    Add dosctrings

--- wakapy/parser.py
+++ wakapy/parser.py
@@ -1,10 +1,28 @@
 import json
 from typing import List
 
-from wakapy.days import Day
+from wakapy.day import Day
 
 
 class JsonDict:
+    """
+    Class that extracts the data from the json file.
+
+    Parameters
+    __________
+
+    fp : :class:`str`
+        file path, received from :class:`.User`
+
+    Attributes
+    ---------
+    fp
+        :class:`str`
+    file
+        :class:`dict`
+        The whole data.
+
+    """
     def __init__(self, fp: str):
         self.fp = fp
         self.file = self._load_file()
@@ -17,10 +35,19 @@ class JsonDict:
 
     @property
     def user_data(self) -> dict:
+        """
+        :property:
+
+        :return: :class:`dict` User's data, doesn't containt any :class:`.Day`
+        """
         return self.file['user']
 
     @property
     def days(self) -> List[Day]:
+        """
+        :property:
+        :return: List[:class:`.Day`] The list of days that will be stored in :class:`.User`
+        """
         return self._days
 
     def __repr__(self):
