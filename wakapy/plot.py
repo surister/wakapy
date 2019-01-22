a50ac4ba3ae56ddfc83e89c233b6eff7e0e5f948
surister <surister98@gmail.com>
2019-01-22 17:59:16 +0100

    Add docstrings

--- wakapy/plot.py
+++ wakapy/plot.py
@@ -1,7 +1,10 @@
 import matplotlib.pyplot as plt
 
 
-def get_labels(l, hours, items):
+def _get_labels(l, hours, items):
+    """
+    Formats the labels, undocumented on purpose, not relevant.
+    """
     total_sum = sum(l)
 
     it = 0
@@ -14,6 +17,24 @@ def get_labels(l, hours, items):
 
 
 class PieChart:
+    """
+    Matplotlib pie chart that comes with the library for the people that just want a quick
+    insight of their data.
+
+    Parameters
+    ----------
+
+    _dict : :class:`dict`
+        The dictionary containing all the data that will be showed in the piechart.
+    num : :class:`int`
+        Number of max elements that will be shown in the chart
+    dates : :class:`datetime.date`
+        The dates if the data comes from a slice. Just for tagging purposes.
+
+    Note
+    ----
+    You are suppose to access this by :meth:`.pie_chart`
+    """
     def __init__(self, _dict, num: int, dates=None):
         self.dict = _dict
         self.fig = None
@@ -25,7 +46,7 @@ class PieChart:
         data1 = data[:num]
 
         hours = list(map(lambda x: f'{round(x, 2)}h', data1))
-        labels = get_labels(data1, hours, item1)
+        labels = _get_labels(data1, hours, item1)
         fig, ax = plt.subplots(figsize=(5, 5), subplot_kw=dict(aspect='equal'))
         wedges, texts = ax.pie(data1)
 
@@ -36,10 +57,19 @@ class PieChart:
                   )
 
     def show(self) -> None:
+        """
+        Shows the figure. :class:`matplotlib.pyplot.show`
+        """
         self.fig = plt.gcf()
         plt.show()
 
     def save(self, fp: str) -> None:
+        """
+        Saves the figure in the given path. :class:`matplotlib.pyplot.save(fp)`
+
+        :param fp:
+            :class:`str`
+        """
         fig = self.fig if self.fig else plt.gcf()
         fig.savefig(fp, bbox_inches='tight', format='png', dpi=100)
 
